23665cafdd8fcc5ee0e94bcf87e1c9af459e356b
mivchal <mivchal@hotmail.com>
2021-06-07 18:00:37 +0200

    Initial commit

---
+++ src/main/java/pl/edu/pb/springmarketplace/SpringMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pb.springmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/pb/springmarketplace/SpringMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pb.springmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
