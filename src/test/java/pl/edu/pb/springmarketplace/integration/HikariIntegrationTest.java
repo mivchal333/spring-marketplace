0ad1fd5ed0e4ca9e2a787c96cc8260a2d5169926
mivchal <mivchal@hotmail.com>
2021-06-15 22:45:54 +0200

    fix hikari integration test

--- src/test/java/pl/edu/pb/springmarketplace/integration/HikariIntegrationTest.java
+++ src/test/java/pl/edu/pb/springmarketplace/integration/HikariIntegrationTest.java
@@ -1,16 +1,11 @@
 package pl.edu.pb.springmarketplace.integration;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-
 import javax.sql.DataSource;
-
-import static org.junit.Assert.assertEquals;
 
 @SpringBootTest(
         properties = "spring.datasource.type=com.zaxxer.hikari.HikariDataSource"
@@ -22,6 +17,6 @@ public class HikariIntegrationTest {
 
     @Test
     public void hikariConnectionPoolIsConfigured() {
-        assertEquals("com.zaxxer.hikari.HikariDataSource", dataSource.getClass().getName());
+        Assertions.assertEquals("com.zaxxer.hikari.HikariDataSource", dataSource.getClass().getName());
     }
 }
